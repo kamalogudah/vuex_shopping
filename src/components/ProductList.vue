c17cac83450950c88e65befbb2440c1391cd793a
kamalogudah <mcpaul2058@gmail.com>
2019-05-01 12:39:22 +0300

    Ensure store access from all our components

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -9,8 +9,6 @@
 </template>
 
 <script>
-import store from '@/store/index';
-
 export default {
   data() {
     return {
@@ -19,13 +17,13 @@ export default {
   },
   computed: {
     products() {
-      return store.getters.availableProducts;
+      return this.$store.getters.availableProducts;
     },
   },
 
   created() {
     this.loading = true;
-    store.dispatch('fetchProducts').then(() => (this.loading = false));
+    this.$store.dispatch('fetchProducts').then(() => (this.loading = false));
   },
 };
 </script>
