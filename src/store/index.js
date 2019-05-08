ab69cb64655e4651341e241f51c185a57b08deba
kamalogudah <mcpaul2058@gmail.com>
2019-05-08 11:39:57 +0300

    Split Vuex Store in Multiple Files

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-import Vuex from 'vuex'
-import Vue from 'vue'
-import shop from '@/api/shop'
+import Vuex from 'vuex';
+import Vue from 'vue';
+import actions from './actions';
 
 
 Vue.use(Vuex)
@@ -41,43 +41,11 @@ export default new Vuex.Store({
             }
         }
     },
-    actions: {
-        fetchProducts({ commit }) {
-            return new Promise((resolve, reject) => {
-                shop.getProducts(products => {
-                    commit('setProducts', products)
-                    resolve()
-                })
 
-            })
-        },
-        addProductToCart({ state, getters, commit }, product) {
-            if (getters.productIsInStock(product)) {
-                // find cartItem
-                const cartItem = state.cart.find(item => item.id === product.id)
-                if (!cartItem) {
-                    commit('pushProductToCart', product.id)
 
-                } else {
-                    commit('incrementItemQuantity', cartItem)
-                }
-                commit('decrementProductInventory', product)
-            }
-        },
-        checkout({ state, commit }) {
-            shop.buyProducts(
-                state.cart,
-                () => {
-                    commit('emptyCart')
-                    commit('setCheckoutStatus', 'success')
-                },
-                () => {
-                    commit('setCheckoutStatus', 'fail')
-                }
-            )
-        }
-
-    },
+    //you can use short hand actions,
+    //actions,
+    actions: actions,
     mutations: {
         setProducts(state, products) {
             //update products
